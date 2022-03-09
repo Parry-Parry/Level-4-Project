cb6bbd3e3573882c5514c234f88652c939bf502a
Andrew Parry <2393265p@student.gla.ac.uk>
2022-03-09 23:02:46 +0000

    Small edit

--- code2text/models/baseline/model.py
+++ code2text/models/baseline/model.py
@@ -183,7 +183,7 @@ class seq2seqTrain(tf.keras.Model):
             dec_state = enc_state
             loss = tf.constant(0.0)
 
-            for t in range(max_target_length.eval()-1):
+            for t in range(max_target_length.shape.num_elements()-1):
                 new_tokens = target_tokens[:, t:t+2]
                 step_loss, dec_state = self._loop_step(new_tokens, input_mask,
                                              enc_output, dec_state)
