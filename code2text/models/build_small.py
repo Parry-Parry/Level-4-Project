c1f64a6b4fd9b83b584a01f8bb0a964479c1a2c0
Andrew Parry <2393265p@student.gla.ac.uk>
2022-03-16 13:28:39 +0000

    Structure change of training

--- code2text/models/build_small.py
+++ code2text/models/build_small.py
@@ -3,7 +3,7 @@ import transformers
 from transformers import RobertaConfig, TFRobertaModel
 
 h=8
-l=6
+l=3
 hs=384
 
 print("building...")
@@ -13,5 +13,5 @@ model.save_pretrained("tmp_roberta")
 
 model = transformers.TFEncoderDecoderModel.from_encoder_decoder_pretrained("tmp_roberta", "tmp_roberta")
 print("saving...")
-model.save_pretrained("smallbert")
+model.save_pretrained("medsmallbert")
 
